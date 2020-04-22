aeb82dfca7b780f9f17f18e3e7e4c2fccd24fe16
Dai <daig@users.noreply.github.com>
2020-04-22 14:15:21 -0700

    reexport boolean functions from Stock

--- src/Stock.hs
+++ src/Stock.hs
@@ -1,4 +1,4 @@
 {-| Description : Stock datatypes and classes -}
 module Stock (module X) where
 import GHC.Types as X (Bool(..),Char(..),Int(..),Word(..),Float(..),Double(..),IO(..),Ordering(..))
-import GHC.Classes as X (Eq(..),Ord(..))
+import GHC.Classes as X (Eq(..),Ord(..),(&&),(||),not)
