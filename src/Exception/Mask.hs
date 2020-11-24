2a82b60a24459d0c73fd4e115abadd0730c73590
Dai <daig@users.noreply.github.com>
2020-11-23 19:36:41 -0800

    Bundle Mask patterns

--- src/Exception/Mask.hs
+++ src/Exception/Mask.hs
@@ -1,5 +1,5 @@
-{-# language PatternSynonyms #-}
-module Exception.Mask where
+module Exception.Mask (State(Unmasked,Uninterruptible,Interruptible)
+                      ,async,uninterruptible,state)where
 import Prelude hiding (State#)
 
 async ∷ IO# a → IO# a
@@ -8,14 +8,14 @@ async = maskAsyncExceptions#
 uninterruptible ∷ IO# a → IO# a
 uninterruptible = maskUninterruptible#
 
-state ∷ IO# State#
-state = getMaskingState#
+state ∷ IO# State
+state = coerce getMaskingState#
 
-type State# = I
-
-pattern Unmasked ∷ State#
-pattern Unmasked = 0#
-pattern Uninterruptible ∷ State#
-pattern Uninterruptible = 1#
-pattern Interruptible ∷ State#
-pattern Interruptible = 2#
+newtype State ∷ TYPE IntRep where State# ∷ I → State
+pattern Unmasked ∷ State
+pattern Unmasked = State# 0#
+pattern Uninterruptible ∷ State
+pattern Uninterruptible = State# 1#
+pattern Interruptible ∷ State
+pattern Interruptible = State# 2#
+{-# complete Unmasked, Uninterruptible, Interruptible #-}
