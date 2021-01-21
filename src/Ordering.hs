5549efdb7661b23697d744b0420c02a5b12f77c1
Dai <daig@users.noreply.github.com>
2021-01-21 13:45:52 -0500

    fix bug in Ordering

--- src/Ordering.hs
+++ src/Ordering.hs
@@ -6,5 +6,5 @@ newtype Ordering ∷ T_I where Ordering# ∷ I → Ordering
 pattern LT ∷ Ordering
 pattern LT ← ((\(Ordering# i) → i < 0# ) → T ) where LT = Ordering# -1#
 pattern GT ← ((\(Ordering# i) → i > 0# ) → T ) where GT = Ordering# 1#
-pattern EQ ← ((\(Ordering# i) → i ≡ 0# ) → T ) where EQ = Ordering# 1#
+pattern EQ ← ((\(Ordering# i) → i ≡ 0# ) → T ) where EQ = Ordering# 0#
 {-# complete LT, GT, EQ #-}
