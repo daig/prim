1e259b775faf77523de72a6f981de36672aaa82e
Dai <dai@Dais-MacBook-Air.local>
2022-09-05 17:53:35 -0400

    add set method to Index class

--- class/A/Index.hs
+++ class/A/Index.hs
@@ -1,5 +1,9 @@
+{-# language CPP #-}
 module A.Index where
 import Prim
+import Do as Prim
+
+
 type (∈) :: forall {r :: RuntimeRep} {r' :: RuntimeRep}. T r -> T r' -> Constraint
 class (x ∷ T r) ∈ (a ∷ T r') where
   index# ∷ a → I {- ^ Offset in elements -} → x
@@ -27,18 +31,35 @@ instance (♭) a ⇒ (a ∷ T_ r) ∈ P a where
 -}
 
 -- | "A.Prim"
-instance (♭) a ⇒ (a ∷ T r) ∈ (A_Unbox a) where
-  index# (Bytes a) = indexA# a
-  read# (Bytes_M ma) = readA# ma
-  write# (Bytes_M ma) = writeA# ma
+#define INST_IDX_UNBOX(A)\
+instance A ∈ (A_Unbox A) where {\
+  index# (Bytes a) = indexA# a ;\
+  read# (Bytes_M ma) = readA# ma ;\
+  write# (Bytes_M ma) = writeA# ma ;\
+  set (Bytes_M m@(M_UnpinnedByteArray# ma)) x s = case getSizeofMutableByteArray# ma s of (# s', n #) -> setB# m 0# n x s' }
+INST_IDX_UNBOX(I)
+INST_IDX_UNBOX(I8)
+INST_IDX_UNBOX(I16)
+INST_IDX_UNBOX(I32)
+INST_IDX_UNBOX(I64)
+INST_IDX_UNBOX(U)
+INST_IDX_UNBOX(U8)
+INST_IDX_UNBOX(U16)
+INST_IDX_UNBOX(U32)
+INST_IDX_UNBOX(U64)
+INST_IDX_UNBOX(Char)
+INST_IDX_UNBOX(Char8)
+INST_IDX_UNBOX(P#)
+-- INST_IDX_UNBOX((P_Stable s))
 
 instance (♭) a ⇒ (a ∷ T r) ∈ Bytes where
   index# = indexB#
   read# = readB#
   write# = writeB#
+--  set = setB#
 
 deriving via Bytes instance (♭) a ⇒ (a ∷ T r) ∈ Bytes_Pinned
-deriving via (A_Unbox (a :: T r)) instance (♭) a ⇒ (a ∷ T r) ∈ (A_Unbox_Pinned a)
+-- deriving via (A_Unbox (a :: T r)) instance (♭) a ⇒ (a ∷ T r) ∈ (A_Unbox_Pinned a)
 
 instance (♭) x ⇒ (x ∷ T r) ∈ P# where
   index# = indexP#
